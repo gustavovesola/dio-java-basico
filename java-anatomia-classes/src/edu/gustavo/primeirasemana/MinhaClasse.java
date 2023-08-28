8bb11e04d8fee210a088d3761a774e9aec7b1788
Gustavo Vesola <vesolagustavo@gmail.com>
2023-08-28 12:35:48 -0300

    anatomia das classes

---
+++ java-anatomia-classes/src/edu/gustavo/primeirasemana/MinhaClasse.java
@@ -0,0 +1,19 @@
+package edu.gustavo.primeirasemana;
+public class MinhaClasse {
+    
+public static void main(String[] args) {
+     
+    String primeiroNome = "Gustavo";
+    String segundoNome = "Vesola"; 
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome); 
+}
+
+}
+
+
