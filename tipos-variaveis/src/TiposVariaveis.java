782cad6587bd36d80e9f23e044fcda53fa039cc6
Gustavo Vesola <vesolagustavo@gmail.com>
2023-08-29 16:40:53 -0300

    exemplo

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    
+    public static void main(String[] args) {
+        int numero = 5;
+        numero = 10;
+        System.out.print(numero);
+
+
+    }
+
+
+
+}
