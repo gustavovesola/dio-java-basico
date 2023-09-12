cdea45cdc3c90e398729fbfd6ba06d81eb81abaf
Gustavo Vesola <vesolagustavo@gmail.com>
2023-09-12 16:38:34 -0300

    att resultado-escolar

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,10 +1,13 @@
 public class ResultadoEscolar {
 
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 5;
 
         if (nota >= 7)
             System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7) // true ou false
+            System.out.println("Prova de recuperacão");
 
         else
             System.out.println("Reprovado");
