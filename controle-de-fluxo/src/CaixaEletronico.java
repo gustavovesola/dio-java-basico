0024778e6a214cd66014c438355cf651ccce9e57
Gustavo Vesola <vesolagustavo@gmail.com>
2023-09-12 16:19:41 -0300

    att caixa-eletronico

--- controle-de-fluxo/src/CaixaEletronico.java
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -2,12 +2,14 @@ public class CaixaEletronico {
 
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 22.0;
 
-        if (valorSolicitado < saldo)
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
 
-        System.out.println(saldo);
+        } else
+            System.out.println("Saldo insuficiente");
 
     }
 
