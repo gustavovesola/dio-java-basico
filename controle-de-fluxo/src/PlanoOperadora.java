124a1a400542af80d27eaf38347db107a125c8f4
Gustavo Vesola <vesolagustavo@gmail.com>
2023-09-13 14:32:22 -0300

    plano-operadora if else

---
+++ controle-de-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,25 @@
+public class PlanoOperadora {
+
+    public static void main(String[] args) {
+        
+        String plano = "J";
+
+        if (plano =="B") {
+            System.out.println("100 MINUTOS DE LIGACÃO");
+        }else if (plano == "M") {
+            System.out.println("100 MINUTOS DE LIGACÃO");
+            System.out.println("WHATSAPP E INSTRAGRAM ILIMITADO");
+        }else if (plano == "T") {
+            System.out.println("100 MINUTOS DE LIGACÃO");
+            System.out.println("WHATSAPP E INSTRAGRAM ILIMITADO + 5 GB YOUTUBE");        
+        }
+        else
+            System.out.println("PLANO NÃO IDENTIFICADO"); 
+                
+        
+        
+             
+    
+    }
+
+}
