459d7e864adccd62b6ec69577320c8af7bb796d3
Gustavo Vesola <vesolagustavo@gmail.com>
2023-09-15 13:08:47 -0300

    exemplo-for-array-2

---
+++ controle-de-fluxo/src/ExemploForArray2.java
@@ -0,0 +1,9 @@
+public class ExemploForArray2 {
+    public static void main(String[] args) {
+        String alunos [] = { "FELIPE", "JONAS", "JULIA", "MARCOS"};
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+    
+}
