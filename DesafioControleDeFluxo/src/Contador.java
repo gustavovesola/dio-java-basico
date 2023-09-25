28bc41e67e2a4fa4dc112275fa5f21a14513e2d4
Gustavo Vesola <vesolagustavo@gmail.com>
2023-09-25 10:28:17 -0300

    desafio-contador

---
+++ DesafioControleDeFluxo/src/Contador.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+// Exceção personalizada para parâmetros inválidos
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("Exceção: " + exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O primeiro parâmetro não pode ser maior que o segundo.");
+        }
+        int contagem = parametroDois - parametroUm;
+        System.out.println("Resultado da contagem: " + contagem);
+    }
+}
+
+
+
